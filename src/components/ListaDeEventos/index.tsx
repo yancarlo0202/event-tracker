29b5970f29e08d658db35b757b89aa3d23f988f9
Yan <yancarlodev@gmail.com>
2025-09-22 22:54:52 -0400

    aula-01

---
+++ src/components/ListaDeEventos/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Evento from '../Evento';
+import Filtro from '../Filtro';
+import style from './ListaDeEventos.module.scss';
+import { useRecoilValue } from 'recoil';
+import { ListaDeEventosState } from '../../state/atom';
+
+const ListaDeEventos: React.FC<{  
+  aoAlterarStatus: (id: number) => void, 
+  aoDeletarEvento: (id: number) => void, 
+  aoFiltroAplicado: (data: Date | null) => void }> = ({  aoDeletarEvento, aoAlterarStatus, aoFiltroAplicado }) => {
+
+    const eventos = useRecoilValue(ListaDeEventosState);
+
+  return (<section>
+    <Filtro aoFiltroAplicado={aoFiltroAplicado} />
+    <div className={style.Scroll}>
+      {eventos.map(evento => (
+        <Evento aoAlterarStatus={aoAlterarStatus} aoDeletarEvento={aoDeletarEvento} evento={evento} key={evento.id} />
+      ))}
+    </div>
+  </section>)
+}
+
+export default ListaDeEventos
